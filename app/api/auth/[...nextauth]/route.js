ad3d1b6054324de2c1389d4a20de4293e5797aaa
Markus Hofmann <mhofmann@testo.de>
2023-09-25 13:38:25 +0200

    test

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,5 @@
-import { connectMongoDB } from "@/lib/mongodb";
-import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
 
 export const authOptions = {
   providers: [
@@ -13,23 +10,8 @@ export const authOptions = {
       async authorize(credentials) {
         const { email, password } = credentials;
 
-        try {
-          await connectMongoDB();
-          const user = await User.findOne({ email });
-
-          if (!user) {
-            return null;
-          }
-
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-
-          if (!passwordsMatch) {
-            return null;
-          }
-
-          return user;
-        } catch (error) {
-          console.log("Error: ", error);
+        if(email === 'mailhfm@gmail.com' && password === '123456') {
+          return { email: 'mailhfm@gmail.com', password: '123456' };
         }
       },
     }),
